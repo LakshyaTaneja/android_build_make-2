AOSPA/android_build

build: add support for specifying build variant in brunch/breakfast
This little modification allows specifying build variant in brunch/
breakfast commands. For example we can use "brunch i9300 user" to build
user variant instead of default userdebug.

When no extra argument is given, userdebug is default.

Change-Id: I935327252098ee74b34a815a023d2ac6b9a53a30
Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

envsetup.sh
@@ -556,6 +556,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
     for f in `/bin/ls vendor/pa/vendorsetup.sh 2> /dev/null`
@@ -575,7 +576,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the PA model name
-            lunch pa_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch pa_$target-$variant
         fi
     fi
     return $?

LakshyaTaneja/android_build_make-2

envsetup.sh
@@ -590,6 +590,7 @@ function brunch()
 function breakfast()
 {
     target=$1
+    local variant=$2
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
     for f in `/bin/ls vendor/mdroid/vendorsetup.sh 2> /dev/null`
@@ -609,7 +610,10 @@ function breakfast()
             lunch $target
         else
             # This is probably just the MDROID model name
-            lunch mdroid_$target-userdebug
+            if [ -z "$variant" ]; then
+                variant="userdebug"
+            fi
+            lunch mdroid_$target-$variant
         fi
     fi
     return $?